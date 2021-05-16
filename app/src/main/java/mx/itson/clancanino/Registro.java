7bfffce3ddd4eb8d9f68249788d2abf375130fee
Perla <perla.duran12@gmail.com>
2021-05-15 23:40:58 -0600

    Registro y login

--- app/src/main/java/mx/itson/clancanino/Registro.java
+++ app/src/main/java/mx/itson/clancanino/Registro.java
@@ -2,7 +2,6 @@ package mx.itson.clancanino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -16,23 +15,23 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class Login extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_registro);
     }
 
-    public void obtenerSesion(String strEmail, String strPassword){
+    public void mandarRegistro(String strEmail, String strPassword, String strName){
+
+        RequestBody name = RequestBody.create(MediaType.parse("text/plain"), strName);
 
         RequestBody email = RequestBody.create(MediaType.parse("text/plain"), strEmail);
 
         RequestBody password = RequestBody.create(MediaType.parse("text/plain"), strPassword);
 
-        Call<Sesion> llamada = RetrofitUtil.obtenerAPI().ingresar(email, password);
+        Call<Sesion> llamada = RetrofitUtil.obtenerAPI().registrar(name, email, password);
 
         llamada.enqueue(new Callback<Sesion>() {
             @Override
@@ -57,24 +56,28 @@ public class Login extends AppCompatActivity {
 
     }
 
-    public void iniciarSesion(View view) {
+    public void registrar(View view) {
+
+        TextView txtName = (TextView)findViewById(R.id.editTextUserName);
+        String name = txtName.getText().toString().trim();
+
         TextView txtEmail = (TextView)findViewById(R.id.editTextUser);
         String email = txtEmail.getText().toString().trim();
         TextView txtPassword = (TextView)findViewById(R.id.editTextPassword);
         String password = txtPassword.getText().toString().trim();
+        TextView txtPassword2 = (TextView)findViewById(R.id.editTextPassword2);
+        String password2 = txtPassword.getText().toString().trim();
 
-        if(email.equals("") || password.equals("")){
+        if(email.equals("") || password.equals("")|| password2.equals("")|| name.equals("")){
             Toast.makeText(getApplicationContext(), "Ingresa la contraseña y correo", Toast.LENGTH_LONG).show();
         }else{
+            if(password2.equals(password)){
+                Toast.makeText(getApplicationContext(), "Registro correcto", Toast.LENGTH_LONG).show();
+                mandarRegistro(email, password, name);
+            }else{
+                Toast.makeText(getApplicationContext(), "Las contraseñas son diferentes", Toast.LENGTH_LONG).show();
+            }
 
-            obtenerSesion(email, password);
         }
-
-
-    }
-
-    public void registro(View view) {
-        Intent i = new Intent(this, Registro.class);
-        startActivity(i);
     }
 }
--- app/src/main/java/mx/itson/clancanino/interfaces/ClanCaninoAPI.java
+++ app/src/main/java/mx/itson/clancanino/interfaces/ClanCaninoAPI.java
@@ -1,13 +1,21 @@
 package mx.itson.clancanino.interfaces;
 
 import mx.itson.clancanino.Entidades.Sesion;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Query;
 
 public interface ClanCaninoAPI {
 
-    @POST("apis/userLog.php")
-    Call<Sesion> ingresar(@Query("email") String email, @Query("password") String password);
+    @Multipart
+    @POST("userLog.php")
+    Call<Sesion> ingresar(@Part("email") RequestBody email, @Part("password") RequestBody  password);
+
+    @Multipart
+    @POST("user-register.php")
+    Call<Sesion> registrar(@Part("name") RequestBody name, @Part("email") RequestBody email, @Part("password") RequestBody  password);
 }
